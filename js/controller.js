6e803aa9c5908719c4183c497ff725d68bb16a9f
Charles ANDRE <andrecharles4@gmail.com>
2017-01-06 18:19:02 +0100

    Vérification formulaire avec angular

--- js/controller.js
+++ js/controller.js
@@ -1,33 +1,43 @@
-var app = angular.module('mainApp', ['ngRoute']);
+(function(angular) {
+	  'use strict';
+var app = angular.module('mainApp', ['ngRoute', 'ngMessages', 'ngPassword']);
 
 app.controller('loginCtrl', function($scope, $location) {
 	$scope.submit = function() {
 		var uemail = $scope.username;
 		var password = $scope.password;
 		
-			$location.path('home.html');	
+		$location.path('home.html');	
 		
 
 	};
 
 
+
+s
 });
 
 app.controller('signupCtrl', function($scope, $http, $location) {
 	$scope.submit = function() {
 		if($scope.password != $scope.password2) {
-    alert("Password don't match");
-  } else {
-    
-  
-		$http.post("php/register.php",{'uname':$scope.name, 'ulastname':$scope.lastname, 'uemail':$scope.email, 'password':$scope.password, 'phone':$scope.phone})
-		.success(function(data, status, headers, config){
-			alert("data inserted successfully");
-		});
+			alert("Password don't match");
+		} else {
+
+
+			$http.post("php/register.php",{'uname':$scope.name, 'ulastname':$scope.lastname, 'uemail':$scope.email, 'password':$scope.password, 'phone':$scope.phone})
+			.success(function(data, status, headers, config){
+				alert("data inserted successfully");
+			});
 			
-		
 
-	};
 
-}
+		};
+
+	}
+
 });
+
+
+})(window.angular);
+
+
